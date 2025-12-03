2c02038b2fff6ddb321714446f0eb294313b6afb
Taiwofolu <Taiwofolu@gmail.com>
2025-12-03 13:24:45 +0100

    0.4.21

--- frontend/src/services/utils.js
+++ frontend/src/services/utils.js
@@ -14,10 +14,10 @@ const utils = {
     return hash;
   },
   async getClusttaVersion() {
-    return `v0.4.20-beta`;
+    return `v0.4.21-beta`;
   },
    async getRawClusttaVersion() {
-    return `0.4.20`;
+    return `0.4.21`;
   },
   base64ToUint8Array(base64) {
     const binary = atob(base64);
