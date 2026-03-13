95ef4eef903bf67b3e21bd6f9c976fc6ecffb510
taiwofolu <taiwofolu@gmail.com>
2026-03-13 19:12:04 +0100

    bump: 0.4.30 -> 0.4.31

--- frontend/src/services/utils.js
+++ frontend/src/services/utils.js
@@ -14,10 +14,10 @@ const utils = {
     return hash;
   },
   async getClusttaVersion() {
-    return `v0.4.30-beta`;
+    return `v0.4.31-beta`;
   },
    async getRawClusttaVersion() {
-    return `0.4.30`;
+    return `0.4.31`;
   },
   base64ToUint8Array(base64) {
     const binary = atob(base64);
